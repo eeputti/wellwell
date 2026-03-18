77b46c6cacb89039ce8fa4e8cc605c2efc196ea9
Eelis Puro <eelispuro@gmail.com>
2026-03-18 13:19:20 +0200

    Initial Commit

---
+++ wellwell/wellwellApp.swift
@@ -0,0 +1,17 @@
+//
+//  wellwellApp.swift
+//  wellwell
+//
+//  Created by Eelis Puro on 18.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct wellwellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
